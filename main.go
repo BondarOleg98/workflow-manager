8668d326c4d39c514056ff95fc47a091bee8e54e
BondarOleg98 <olegb5195@gmail.com>
2025-02-11 22:51:50 +0200

    [#2] - added a base workflow routing

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	route "./routing"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Test")
+	route.WorkflowEndpoints{}.BaseController()
+	err := http.ListenAndServe(":8080", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
