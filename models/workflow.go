6f783b7b29831de1e131b4bfe0e7f36b2dd4fa91
BondarOleg98 <olegb5195@gmail.com>
2025-03-11 22:48:06 +0200

    [#7] - commented unused fields in the workflow structure

--- models/workflow.go
+++ models/workflow.go
@@ -1,10 +1,8 @@
 package models
 
-import "time"
-
 type Workflow struct {
-	name       string
-	workflowId string
-	createdAt  time.Time
-	updatedAt  time.Time
+	//name       string
+	//workflowId string
+	//createdAt  time.Time
+	//updatedAt  time.Time
 }
