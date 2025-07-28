0bd595582c500ea35447f260fcca764e83c7b623
BondarOleg98 <olegb5195@gmail.com>
2025-07-28 16:02:03 +0300

    [#41] - added an exporting envs into the removing db script

--- app/db/postgres/remove_db_schema.sh
+++ app/db/postgres/remove_db_schema.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+exportEnvs() {
+  logInfo "Exporting envs"
+  envs=$(awk 'NF {print $1 $2}' ../../resources/env.yaml | sed 's/:/=/')
+  for env in ${envs}; do
+    eval "export ${env}"
+  done
+}
+
 removeTablesAndRelationships() {
   local pg_database_connect
   pg_database_connect="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${HOST}:${PORT}/${POSTGRES_DB}"
-  psql ${pg_database_connect} -f delete_schema.sql
+  psql "${pg_database_connect}" -f delete_schema.sql
 }
 
+exportEnvs
 removeTablesAndRelationships
