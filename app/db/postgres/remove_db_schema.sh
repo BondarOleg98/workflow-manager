d968ff153149b2791651c27ed5bd26bd7aa1baf5
BondarOleg98 <olegb5195@gmail.com>
2025-07-28 16:02:28 +0300

    [#41] - added the logging into the removing db script

--- app/db/postgres/remove_db_schema.sh
+++ app/db/postgres/remove_db_schema.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+logInfo() {
+  printf '%b' "\e[32mINFO [$(date '+%Y-%m-%d %H:%M:%S')] $*\e[0m\n"
+}
+
 exportEnvs() {
   logInfo "Exporting envs"
   envs=$(awk 'NF {print $1 $2}' ../../resources/env.yaml | sed 's/:/=/')
@@ -9,10 +13,12 @@ exportEnvs() {
 }
 
 removeTablesAndRelationships() {
+  logInfo "Removing tables and relationships"
   local pg_database_connect
   pg_database_connect="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${HOST}:${PORT}/${POSTGRES_DB}"
   psql "${pg_database_connect}" -f delete_schema.sql
 }
 
+logInfo "Starting the removing script"
 exportEnvs
 removeTablesAndRelationships
