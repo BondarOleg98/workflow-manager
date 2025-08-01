921035a830d2f9aa325cde5df13986d29b7ac6ab
BondarOleg98 <olegb5195@gmail.com>
2025-08-01 20:21:16 +0300

    [#23] - removed the testing of the removing several database instances

--- test/integration/databaseadapter_test.go
+++ test/integration/databaseadapter_test.go
@@ -25,33 +25,23 @@ func TestInitNonExistedDatabaseInstance(test *testing.T) {
 	}
 }
 
-func TestSeveralDatabaseInstances(test *testing.T) {
-	const driverName string = "postgres"
-	const firstConnectionDbUrl string = "postgres://postgres:postgres@localhost/workflow_manager?sslmode=disable"
-	const secondConnectionDbUrl string = "postgres://postgres:postgres@localhost/postgres?sslmode=disable"
-	firstDatabaseActualName, _ := getActualDatabaseNameThroughAdapter(driverName, firstConnectionDbUrl)
-	secondDatabaseActualName, _ := getActualDatabaseNameThroughAdapter(driverName, secondConnectionDbUrl)
-	assert.Equal(test, "workflow_manager", firstDatabaseActualName)
-	assert.Equal(test, "postgres", secondDatabaseActualName)
-}
-
 func getActualDatabaseNameThroughAdapter(driverName string, connectionUrl string) (string, error) {
 	var databaseActualName string
 	pool := db.Pool{
 		DriverName:    driverName,
 		ConnectionUrl: connectionUrl,
 	}
+	defer func() {
+		err := db.CloseDatabaseConnection()
+		if err != nil {
+			return
+		}
+	}()
 	databaseActual := db.InitDatabaseInstance(pool)
 	err := databaseActual.QueryRow("SELECT current_database()").Scan(&databaseActualName)
 	if err != nil {
 		return "", err
 	}
-	defer func(databaseActual *sql.DB) {
-		err := databaseActual.Close()
-		if err != nil {
-			return
-		}
-	}(databaseActual)
 	return databaseActualName, nil
 }
 
