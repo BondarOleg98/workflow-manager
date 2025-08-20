61f9595e9ff7844b984deeeabc11a1c25c7fa573
BondarOleg98 <olegb5195@gmail.com>
2025-08-20 20:15:41 +0300

    [#46] - added the logic of the hashing token

---
+++ app/auth/utils/password.go
@@ -0,0 +1,15 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MaxCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedBytes), err
+}
+
+func VerifyPassword(hashedPassword, providedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(providedPassword))
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.23.10
 require (
 	github.com/lib/pq v1.10.9
 	github.com/oklog/ulid/v2 v2.1.1
+	golang.org/x/crypto v0.41.0
 	gopkg.in/yaml.v3 v3.0.1
 )
