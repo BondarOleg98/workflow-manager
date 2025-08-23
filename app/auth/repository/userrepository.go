e1cc1e2bc50b5cf8f7e316a4c2ac128165a6eb1b
BondarOleg98 <olegb5195@gmail.com>
2025-08-23 22:19:27 +0300

    [#46] - added the base logic of the auth (register and login)

---
+++ app/auth/repository/userrepository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"database/sql"
+	"github.com/oklog/ulid/v2"
+	"time"
+	"workflowmanager/app/auth/models"
+	"workflowmanager/app/db/queries"
+)
+
+type UserRepository struct {
+	database *sql.DB
+}
+
+func NewUserRepository(database *sql.DB) *UserRepository {
+	return &UserRepository{database: database}
+}
+
+func (userRepository *UserRepository) CreateUser(newUser models.User) (*models.User, error) {
+	newUser.CreatedAt = time.Now()
+	newUser.ID = ulid.Make()
+
+	_, err := userRepository.database.Exec(
+		queries.InsertUserQuery, newUser.ID, newUser.Email, newUser.Username, newUser.Password, newUser.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &newUser, nil
+}
+
+func (userRepository *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+
+	err := userRepository.database.QueryRow(
+		queries.SelectUserByEmail, email).Scan(
+		&user,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
---
+++ app/auth/services/authservice.go
@@ -0,0 +1,106 @@
+package services
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+	"workflowmanager/app/auth/models"
+	"workflowmanager/app/auth/repository"
+	"workflowmanager/app/auth/utils"
+)
+
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidToken       = errors.New("invalid token")
+	ErrExpiredToken       = errors.New("token has expired")
+	ErrEmailInUse         = errors.New("email already in use")
+)
+
+type AuthService struct {
+	userRepository *repository.UserRepository
+	jwtToken       []byte
+	accessTokenTTL time.Duration
+}
+
+func NewAuthService(
+	userRepository *repository.UserRepository,
+	jwtToken []byte,
+	accessTokenTTL time.Duration) *AuthService {
+	return &AuthService{
+		userRepository: userRepository,
+		jwtToken:       jwtToken,
+		accessTokenTTL: accessTokenTTL,
+	}
+}
+
+func (authService *AuthService) Register(user models.User) (*models.User, error) {
+	_, err := authService.userRepository.GetUserByEmail(user.Email)
+	if err != nil {
+		return nil, ErrEmailInUse
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return nil, err
+	}
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return nil, err
+	}
+	user.Password = hashedPassword
+	createdUser, err := authService.userRepository.CreateUser(user)
+	if err != nil {
+		return nil, err
+	}
+	return createdUser, nil
+}
+
+func (authService *AuthService) Login(user models.User) (string, error) {
+	retrievedUser, err := authService.userRepository.GetUserByEmail(user.Email)
+	if err != nil {
+		return "", ErrInvalidCredentials
+	}
+	if err := utils.VerifyPassword(retrievedUser.Password, user.Password); err != nil {
+		return "", ErrInvalidCredentials
+	}
+	jwtToken, err := authService.generateAccessToken(retrievedUser)
+	if err != nil {
+		return "", err
+	}
+	return jwtToken, nil
+}
+
+func (authService *AuthService) generateAccessToken(user *models.User) (string, error) {
+	expirationTime := time.Now().Add(authService.accessTokenTTL)
+	claims := jwt.MapClaims{
+		"sub":      user.ID.String(),
+		"username": user.Username,
+		"email":    user.Email,
+		"exp":      expirationTime.Unix(),
+		"iat":      time.Now().Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(authService.jwtToken)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func (authService *AuthService) ValidateToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, valid := token.Method.(*jwt.SigningMethodHMAC); !valid {
+			return nil, ErrInvalidToken
+		}
+		return authService.jwtToken, nil
+	})
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, ErrInvalidToken
+}
