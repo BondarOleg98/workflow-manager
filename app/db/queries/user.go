b2f474a19888faeb26af3b84cf1876d0e17bfc4a
Oleh Bondar <olegb5195@gmail.com>
2025-11-24 21:24:24 +0200

    Fixed auth (#73)
    * [#72] - split user and refresh token db's queries
    
    * [#72] - fixed parameter naming in the user repository

---
+++ app/db/queries/user.go
@@ -0,0 +1,5 @@
+package queries
+
+const SaveUserQuery = "INSERT INTO users (id, email, username, password, created_at) VALUES ($1, $2, $3, $4, $5)"
+const GetUserByEmailQuery = "SELECT * FROM users WHERE email = $1"
+const GetUserByIdQuery = "SELECT id, email, username, password, created_at, last_login FROM users WHERE id = $1"
