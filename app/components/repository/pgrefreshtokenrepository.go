3456b29bc05df1412292e272ed389b97e78f01f2
Oleh Bondar <olegb5195@gmail.com>
2025-11-27 21:23:43 +0200

    Fixed auth refresh token code (#74)
    * [#72] - split user and refresh token db's queries
    
    * [#72] - fixed parameter naming in the user repository
    
    * [#72] - fixed parameter naming in the refresh token repository

--- app/components/repository/pgrefreshtokenrepository.go
+++ app/components/repository/pgrefreshtokenrepository.go
@@ -1,11 +1,47 @@
 package repository
 
 import (
+	"database/sql"
+	"workflowmanager/app/db/queries"
 	"workflowmanager/app/models"
 )
 
-type RefreshTokenRepository interface {
-	CreateRefreshToken(token models.RefreshToken) error
-	GetRefreshToken(token string) (*models.RefreshToken, error)
-	RevokeRefreshToken(refreshToken string) error
+type PostgresRefreshTokenRepository struct {
+	database *sql.DB
+}
+
+func NewPostgresRefreshTokenRepository(database *sql.DB) *PostgresRefreshTokenRepository {
+	return &PostgresRefreshTokenRepository{database: database}
+}
+
+func (postgresRefreshTokenRepository *PostgresRefreshTokenRepository) CreateRefreshToken(refreshToken models.RefreshToken) error {
+	_, err := postgresRefreshTokenRepository.database.Exec(queries.InsertRefreshTokenQuery,
+		refreshToken.Id.String(),
+		refreshToken.UserId.String(),
+		refreshToken.Token,
+		refreshToken.ExpiredAt,
+		refreshToken.CreatedAt,
+		refreshToken.Revoked)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (postgresRefreshTokenRepository *PostgresRefreshTokenRepository) GetRefreshToken(token string) (*models.RefreshToken, error) {
+	var retrievedToken models.RefreshToken
+	err := postgresRefreshTokenRepository.database.QueryRow(queries.GetRefreshTokenQuery, token).Scan(
+		&retrievedToken.Id, &retrievedToken.UserId,
+		&retrievedToken.Token, &retrievedToken.ExpiredAt,
+		&retrievedToken.CreatedAt, &retrievedToken.Revoked,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &retrievedToken, nil
+}
+
+func (postgresRefreshTokenRepository *PostgresRefreshTokenRepository) RevokeRefreshToken(refreshToken string) error {
+	_, err := postgresRefreshTokenRepository.database.Exec(queries.RevokedRefreshTokenQuery, refreshToken)
+	return err
 }
