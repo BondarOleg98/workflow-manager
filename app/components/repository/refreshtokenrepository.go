815a678ddf554f58c5b28ec832cfb018de9c8520
BondarOleg98 <olegb5195@gmail.com>
2025-11-27 21:22:44 +0200

    [#72] - fixed parameter naming in the refresh token repository

--- app/components/repository/refreshtokenrepository.go
+++ app/components/repository/refreshtokenrepository.go
@@ -1,47 +1,11 @@
 package repository
 
 import (
-	"database/sql"
-	"workflowmanager/app/db/queries"
 	"workflowmanager/app/models"
 )
 
-type PostgresRefreshTokenRepository struct {
-	database *sql.DB
-}
-
-func NewPostgresRefreshTokenRepository(database *sql.DB) *PostgresRefreshTokenRepository {
-	return &PostgresRefreshTokenRepository{database: database}
-}
-
-func (PostgresRefreshTokenRepository *PostgresRefreshTokenRepository) CreateRefreshToken(refreshToken models.RefreshToken) error {
-	_, err := PostgresRefreshTokenRepository.database.Exec(queries.InsertRefreshTokenQuery,
-		refreshToken.Id.String(),
-		refreshToken.UserId.String(),
-		refreshToken.Token,
-		refreshToken.ExpiredAt,
-		refreshToken.CreatedAt,
-		refreshToken.Revoked)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (PostgresRefreshTokenRepository *PostgresRefreshTokenRepository) GetRefreshToken(token string) (*models.RefreshToken, error) {
-	var retrievedToken models.RefreshToken
-	err := PostgresRefreshTokenRepository.database.QueryRow(queries.GetRefreshTokenQuery, token).Scan(
-		&retrievedToken.Id, &retrievedToken.UserId,
-		&retrievedToken.Token, &retrievedToken.ExpiredAt,
-		&retrievedToken.CreatedAt, &retrievedToken.Revoked,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return &retrievedToken, nil
-}
-
-func (PostgresRefreshTokenRepository *PostgresRefreshTokenRepository) RevokeRefreshToken(refreshToken string) error {
-	_, err := PostgresRefreshTokenRepository.database.Exec(queries.RevokedRefreshTokenQuery, refreshToken)
-	return err
+type RefreshTokenRepository interface {
+	CreateRefreshToken(token models.RefreshToken) error
+	GetRefreshToken(token string) (*models.RefreshToken, error)
+	RevokeRefreshToken(refreshToken string) error
 }
