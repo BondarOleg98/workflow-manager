c1a95c6782fbf9e602d1dd859f3e083d0f58327a
Oleh Bondar <olegb5195@gmail.com>
2026-01-06 22:04:14 +0200

    Fixed the issue with the workflow service test (#77)
    * [#66] - added a new test lib
    
    * [#66] - upper the go lib version
    
    * [#66] - fixed the issue when the db is not empty
    
    * Increase the builder and linter version
    
    * Use the 6 version of the linter
    
    * Use the latest version of the linter
    
    * Updated the go version matrix
    
    * Fixed the ineffectual assignment to err

--- go.mod
+++ go.mod
@@ -1,11 +1,19 @@
 module workflowmanager
 
-go 1.23.10
+go 1.24.0
+
+toolchain go1.24.4
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
-	golang.org/x/crypto v0.41.0
+	github.com/stretchr/testify v1.11.1
+	golang.org/x/crypto v0.46.0
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
--- test/integration/workflowservice_test.go
+++ test/integration/workflowservice_test.go
@@ -1,106 +1,101 @@
 package integration
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"workflowmanager/app/components/services"
 	"workflowmanager/app/models"
 )
 
-type WorkflowServiceTest struct {
-	workflowService *services.WorkflowService
-}
-
 const pageSizeSeveralElements int = 2
 const pageSizeOneElement int = 1
-const secondWorkflowIndex int = 1
 
 var expectedWorkflows = []models.Workflow{
-	{Name: "first_test"},
-	{Name: "second_test"},
+	{
+		Name:  "first_workflow",
+		State: models.CREATED,
+		Tasks: []models.Task{
+			{
+				Name:  "first_task",
+				State: models.CREATED,
+			},
+			{
+				Name:  "second_task",
+				State: models.CREATED,
+			},
+		},
+	},
+	{
+		Name:  "second_workflow",
+		State: models.CREATED,
+		Tasks: []models.Task{
+			{
+				Name:  "first_task",
+				State: models.CREATED,
+			},
+		},
+	},
+}
+
+type WorkflowServiceTest struct {
+	workflowService *services.WorkflowService
 }
 
 func NewWorkflowServiceTest(workflowService *services.WorkflowService) *WorkflowServiceTest {
 	return &WorkflowServiceTest{workflowService: workflowService}
 }
 
-// TODO: need to change according to pagination using filter
 func (workflowServiceTest *WorkflowServiceTest) testCrdOperations(test *testing.T) {
+	workflowServiceTest.testSaveWorkflow(test)
+	workflowServiceTest.testGetWorkflowsByPaginationWithoutCursor(test)
+	workflowServiceTest.testGetWorkflowsByPaginationUsingCursor(test)
+	workflowServiceTest.testRemoveWorkflowById(test)
+}
+
+func (workflowServiceTest *WorkflowServiceTest) testSaveWorkflow(test *testing.T) {
 	for _, workflow := range expectedWorkflows {
-		workflowServiceTest.testSaveWorkflow(test, workflow)
-	}
-	workflowServiceTest.testGetWorkflowsByPaginationWithoutCursor(test, pageSizeSeveralElements, expectedWorkflows)
-	workflowServiceTest.testGetWorkflowsByPaginationWithoutCursor(test, pageSizeOneElement, expectedWorkflows)
-	gotWorkflowsByPaginationWithoutCursor, err :=
-		workflowServiceTest.workflowService.GetWorkflowsByPagination("", pageSizeSeveralElements)
-	if err != nil {
-		test.Errorf("the issue during getting workflows without cursor")
-	}
-	cursorSecondElement := gotWorkflowsByPaginationWithoutCursor[secondWorkflowIndex].WorkflowId
-	workflowServiceTest.testGetWorkflowsByPaginationUsingCursor(test,
-		cursorSecondElement.String(), pageSizeOneElement, expectedWorkflows[secondWorkflowIndex:])
-	for _, workflow := range gotWorkflowsByPaginationWithoutCursor {
-		workflowServiceTest.testRemoveWorkflowById(test, workflow)
+		err := workflowServiceTest.workflowService.SaveWorkflow(workflow)
+		assert.Nil(test, err, "the error during saving the workflow")
+		gotWorkflowByPaginationWithoutCursor, _ :=
+			workflowServiceTest.workflowService.GetWorkflowsByPagination("", pageSizeOneElement)
+		assert.Equal(test, workflow.Name, gotWorkflowByPaginationWithoutCursor[0].Name,
+			"the data between expected and actual workflows are different")
+		assert.Equal(test, workflow.State, gotWorkflowByPaginationWithoutCursor[0].State,
+			"the data between expected and actual workflows are different")
 	}
 }
 
-func (workflowServiceTest *WorkflowServiceTest) testSaveWorkflow(
-	test *testing.T, workflow models.Workflow) {
-	err := workflowServiceTest.workflowService.SaveWorkflow(workflow)
-	assertEqualNil(test, err, "the error during saving the workflow")
-}
-
-func (workflowServiceTest *WorkflowServiceTest) testGetWorkflowsByPaginationWithoutCursor(
-	test *testing.T,
-	pageSizeElements int, expectedWorkflows []models.Workflow) {
+func (workflowServiceTest *WorkflowServiceTest) testGetWorkflowsByPaginationWithoutCursor(test *testing.T) {
+	var cursor string
 	gotWorkflowsByPaginationWithoutCursor, err :=
-		workflowServiceTest.workflowService.GetWorkflowsByPagination("", pageSizeElements)
-	assertEqualNil(test, err, "the error during getting workflows by pagination without cursor")
-	assertEqual(test, len(gotWorkflowsByPaginationWithoutCursor), pageSizeElements,
+		workflowServiceTest.workflowService.GetWorkflowsByPagination(cursor, pageSizeSeveralElements)
+	assert.Nil(test, err, "the error during getting workflows by pagination without cursor")
+	assert.Equal(test, len(gotWorkflowsByPaginationWithoutCursor), pageSizeSeveralElements,
 		"the count of the workflows are different after getting by pagination without cursor")
-	for indexWorkflow, workflow := range gotWorkflowsByPaginationWithoutCursor {
-		assertEqual(test, expectedWorkflows[indexWorkflow].Name, workflow.Name,
-			"the data between expected and actual workflows are different")
-	}
 }
 
-func (workflowServiceTest *WorkflowServiceTest) testGetWorkflowsByPaginationUsingCursor(
-	test *testing.T, cursor string, pageSizeElements int, expectedWorkflows []models.Workflow) {
-	gotWorkFLowByPaginationUsingCursor, err :=
-		workflowServiceTest.workflowService.GetWorkflowsByPagination(cursor, pageSizeElements)
-	assertEqualNil(test, err, "the error during getting workflows by pagination using cursor")
-	assertEqual(test, len(gotWorkFLowByPaginationUsingCursor), pageSizeElements,
+func (workflowServiceTest *WorkflowServiceTest) testGetWorkflowsByPaginationUsingCursor(test *testing.T) {
+	var cursor string
+	gotWorkflowsByPaginationWithoutCursor, _ :=
+		workflowServiceTest.workflowService.GetWorkflowsByPagination(cursor, pageSizeOneElement)
+	gotWorkflowsByPaginationUsingCursor, err :=
+		workflowServiceTest.workflowService.GetWorkflowsByPagination(
+			gotWorkflowsByPaginationWithoutCursor[0].WorkflowId.String(), pageSizeSeveralElements)
+	assert.Nil(test, err, "the error during getting workflows by pagination using cursor")
+	assert.Equal(test, len(gotWorkflowsByPaginationUsingCursor), pageSizeOneElement,
 		"the count of the workflows are different after getting by pagination using cursor")
-	for indexWorkflow, workflow := range gotWorkFLowByPaginationUsingCursor {
-		assertEqual(test, expectedWorkflows[indexWorkflow].Name, workflow.Name,
-			"the data between expected and actual workflows are different")
-	}
+	assert.Equal(test, expectedWorkflows[1].Name, gotWorkflowsByPaginationUsingCursor[0].Name)
+	assert.Equal(test, expectedWorkflows[1].State, gotWorkflowsByPaginationUsingCursor[0].State)
 }
 
-func (workflowServiceTest *WorkflowServiceTest) testRemoveWorkflowById(test *testing.T, removingWorkflow models.Workflow) {
-	err := workflowServiceTest.workflowService.RemoveWorkflowById(removingWorkflow.WorkflowId.String())
-	assertEqualNil(test, err, "the error during removing the workflow by id")
-	_, err = workflowServiceTest.workflowService.GetWorkflowById(removingWorkflow.WorkflowId.String())
-	assertEqualNotNil(test, err, "the workflow by id was not deleted")
-}
-
-func assertEqualNil(test *testing.T, err error, message string) {
-	if err != nil {
-		test.Errorf("%s", message)
-	}
-}
-
-func assertEqualNotNil(test *testing.T, err error, message string) {
-	if err == nil {
-		test.Errorf("%s", message)
-	}
-}
-
-func assertEqual(test *testing.T, expected any, actual any, message string) {
-	if reflect.TypeOf(expected) != reflect.TypeOf(actual) {
-		test.Errorf("the expected value and actual value have the different types")
-	}
-	if expected != actual {
-		test.Errorf("%s", message)
+func (workflowServiceTest *WorkflowServiceTest) testRemoveWorkflowById(test *testing.T) {
+	var cursor string
+	gotWorkflowsByPaginationUsingCursor, _ :=
+		workflowServiceTest.workflowService.GetWorkflowsByPagination(cursor, pageSizeSeveralElements)
+	for _, workflow := range gotWorkflowsByPaginationUsingCursor {
+		err := workflowServiceTest.workflowService.RemoveWorkflowById(workflow.WorkflowId.String())
+		assert.Nil(test, err, "the error during removing the workflow by id")
+		_, err = workflowServiceTest.workflowService.GetWorkflowById(workflow.WorkflowId.String())
+		assert.NotNil(test, err, "the workflow by id was not deleted")
 	}
 }
