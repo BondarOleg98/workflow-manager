ec0cc2322bc6b8c8fd6dcf29c6cd9026b3b249da
BondarOleg98 <olegb5195@gmail.com>
2026-02-27 22:59:34 +0200

    Fixed tests according to the new logic

--- test/unit/config_test.go
+++ test/unit/config_test.go
@@ -8,10 +8,7 @@ import (
 
 func TestLoadExistedConfigs(test *testing.T) {
 	const existedConfigFilePath string = "../../app/resources/dev_env.yaml"
-	err := util.LoadConfigs(existedConfigFilePath)
-	if err != nil {
-		test.FailNow()
-	}
+	util.LoadConfigs(existedConfigFilePath)
 	if os.Getenv("POSTGRES_USER") != "postgres" ||
 		os.Getenv("SERVICE_PORT") != "8080" {
 		test.Errorf("the configs have diference values")
