03a2ae51e3d8f17b085b82b492328e728c94a412
BondarOleg98 <olegb5195@gmail.com>
2026-04-11 13:47:26 +0300

    Fixed the issue when token expires

--- app/db/queries/refreshtoken.go
+++ app/db/queries/refreshtoken.go
@@ -4,3 +4,4 @@ const InsertRefreshTokenQuery = "INSERT INTO refresh_tokens (user_id, token, exp
 const GetRefreshTokenByValueQuery = "SELECT id, user_id, token, expired_at, created_at, revoked FROM refresh_tokens WHERE token = $1"
 const GetRefreshTokenByUserIdQuery = "SELECT id, user_id, token, expired_at, created_at, revoked FROM refresh_tokens WHERE user_id = $1"
 const RevokedRefreshTokenQuery = "UPDATE refresh_tokens SET revoked = true WHERE token = $1"
+const RemoveRefreshTokenQuery = "DELETE FROM refresh_tokens WHERE token = $1"
